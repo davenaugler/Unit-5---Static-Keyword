3238b5255c997cef3b6bba35ff8e572dd2d25cf7
Dave Naugler <davenaugler@gmail.com>
2023-02-08 15:26:46 -0700

    Initial commit for Unit 5 Lesson 1

---
+++ src/com/coderscampus/lesson1/StaticExample.java
@@ -0,0 +1,17 @@
+package com.coderscampus.lesson1;
+
+public class StaticExample {
+	
+	// static = belongs to the class, not the instance of the class
+	// non-static = belongs to an instance of the class (not the class itself)
+	
+	int someNumber = 10;
+
+	public static void main(String[] args) {
+		if (someNumber > 11) {
+			System.out.println("say something");
+		}
+
+	}
+
+}
